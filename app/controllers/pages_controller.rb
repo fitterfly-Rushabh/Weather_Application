094e50e1d52704a67728c34c166d53e52e1ceece
rushabh-ff <rushabh.dave@fitterfly.in>
2022-07-29 22:24:23 +0530

    getting params from input

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,9 @@
 class PagesController < ApplicationController
     def index
-
-        url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?city=Nadiad"
-
-        headers = {
-            "X-RapidAPI-Host"  => "weatherbit-v1-mashape.p.rapidapi.com",
-            "X-RapidAPI-Key" => "8a8c80b7e3msh5f4ffa420b51379p153c4ejsn4fbccd00fa2d"
-        }
-
-        @response = HTTParty.get(url, :headers => headers)
-        # render json: @response
     end
 
     def data
-        url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?city=Nadiad"
+        url = "https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly?city=#{params[:query]}"
 
         headers = {
             "X-RapidAPI-Host"  => "weatherbit-v1-mashape.p.rapidapi.com",
@@ -21,6 +11,5 @@ class PagesController < ApplicationController
         }
 
         @response = HTTParty.get(url, :headers => headers)
-        # render json: @response
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "pages#index"
-  get 'data', to: 'pages#data'
+  root "pages#data"
+  get 'search', to: 'pages#data'
 
 end
